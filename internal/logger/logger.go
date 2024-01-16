8b7c85ca97939b130cd030b6ac97ca103f8fd113
Bocabbage <378066505@qq.com>
2024-01-16 17:13:09 +0800

    Update logger.

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -7,3 +7,23 @@ var logger *standardLogger
 func init() {
 	logger = newLogger()
 }
+
+func Error(v ...interface{}) {
+	logger.error(v...)
+}
+
+func Warn(v ...interface{}) {
+	logger.warn(v...)
+}
+
+func Debug(v ...interface{}) {
+	logger.debug(v...)
+}
+
+func Info(v ...interface{}) {
+	logger.info(v...)
+}
+
+func Fatal(v ...interface{}) {
+	logger.fatal(v...)
+}
