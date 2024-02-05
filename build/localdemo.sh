155be8d576823ed092b10bfafd5eefc7243dffb3
Bocabbage <378066505@qq.com>
2024-02-05 17:59:02 +0800

    Update for cookie check.

--- build/localdemo.sh
+++ build/localdemo.sh
@@ -1,4 +1,6 @@
+docker rmi localdemo-web
 mv config/example.env config/.env && \
 cd deployments/docker/localdemo && \
 docker-compose -f docker-compose.yaml up -d && \
+cd ../../../ && \
 mv config/.env config/example.env
